a6c8e5481cd339565aab1ad7ca7e694f2e1422fc
shivain07 <shivaingusain007@gmail.com>
2022-07-30 00:02:01 +0530

    added comments for clearilty

--- Recursion/waysToClimbStairs.js
+++ Recursion/waysToClimbStairs.js
@@ -1,3 +1,15 @@
+
+/**
+ * @recursion fanda -->
+ * 
+ * chotaProb =fx
+ * bdaprob=fx+something
+ * return badaproblem
+ * 
+ * 
+ * must include @base case and return statement
+ */
+
 /**
  * 
  * @ques There are n stairs, a person standing at the bottom wants to reach the top. The person can climb either 1 stair or 2 stairs at a time. Count the number of ways, the person can reach the top (order does matter).
@@ -8,13 +20,10 @@
 //way 1 recursion <can be more optimizsed>
 const countWay = (n) => {
     if (n < 0) {
-        return 0;
+        return 0;  //if no step is there then return 0
     }
     if (n == 0) {
-        return 1;
-    }
-    if (n == 1) {
-        return 1;
+        return 1; // if n <no.ofstep> == 0 it signify that there is no step to climb the person still have a step though the one in which he is standing
     }
     let fn = countWay(n - 1) + countWay(n - 2);
     return fn;
